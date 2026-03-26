334af1b27a440c24c26209d2ac9599f2da14642f
paralodox1-hue <paralodox1@gmail.com>
2026-03-26 13:19:31 +0530

    UC5 implemented

--- HelloApp.java
+++ HelloApp.java
@@ -1,13 +1,14 @@
 public class HelloApp {
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("Hello World");
-        } 
-        else {
+        // Check if arguments are provided
+        if (args.length > 0) {
+            // Enhanced for loop to greet each argument
             for (String name : args) {
-                System.out.println("Hello " + name);
+                System.out.println("Hello, " + name + "!");
             }
-
+        } else {
+            // Default greeting when no arguments are given
+            System.out.println("Hello, World!");
         }
     }
 }
