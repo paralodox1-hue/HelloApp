86e17bbb0d9eba15657df91a0a5156678ffe8e39
paralodox1-hue <paralodox1@gmail.com>
2026-03-26 13:29:34 +0530

    UC7 implementation

--- HelloApp.java
+++ HelloApp.java
@@ -4,17 +4,9 @@ public class HelloApp {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            StringBuilder greeting = new StringBuilder("Hello, ");
             
-             
-            for (String name : args) {
-                greeting.append(name).append(", ");
-            }
-            
-             
-            String finalGreeting = greeting.substring(0, greeting.length() - 2);
-            
-            System.out.println(finalGreeting);
+            String names = String.join(", ", args);
+            System.out.println("Hello, " + names);
         }
     }
 }
